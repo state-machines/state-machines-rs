c8f3aa126d2bf496e4a54c312689b31b8321cf0c
metasharp <1176758572@qq.com>
2026-06-13 07:51:32 +0800

    增加new_init_state函数

---
+++ examples/traffic_light/src/main.rs
@@ -0,0 +1,39 @@
+use state_machines::state_machine;
+
+state_machine! {
+    name: TrafficLight,
+    dynamic: true,
+    initial: Red,
+    states: [Red, Yellow, Green],
+    events {
+        next {
+            transition: { from: Red, to: Green }
+            transition: { from: Green, to: Yellow }
+            transition: { from: Yellow, to: Red }
+        }
+    }
+}
+
+fn main() {
+    // Create dynamic machine in a caller-selected state
+    // let mut light = DynamicTrafficLight::new_init_state((), TrafficLightState::Yellow);
+    let mut light = DynamicTrafficLight::new(());
+
+    let a = TrafficLightState::Yellow;
+
+    let b = TrafficLightEvent::Next;
+
+    let a = light.current_state();
+    println!("{:?}", a);
+
+    return;
+    // Runtime event dispatch
+    light.handle(TrafficLightEvent::Next).unwrap();
+    assert_eq!(light.current_state(), TrafficLightState::Red);
+
+    light.handle(TrafficLightEvent::Next).unwrap();
+    assert_eq!(light.current_state(), TrafficLightState::Green);
+
+    light.handle(TrafficLightEvent::Next).unwrap();
+    assert_eq!(light.current_state(), TrafficLightState::Yellow);
+}
